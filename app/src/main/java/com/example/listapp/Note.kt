aa32005f3426209730a489a0c37aa5ce02077410
snehalnarkar <snehalnarkar30@gmail.com>
2022-08-03 19:25:15 +0530

    room database

---
+++ app/src/main/java/com/example/listapp/Note.kt
@@ -0,0 +1,9 @@
+package com.example.listapp
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "note_table")
+data class Note(val title: String,
+                val description: String,
+                val priority: Int,
+                @PrimaryKey(autoGenerate = false) val id: Int? = null)
